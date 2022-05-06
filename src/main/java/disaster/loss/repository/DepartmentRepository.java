14657fe6846c58fc4d4a88dfae3f71b7fa09b371
Lawrence Chirowodza <chidsza@gmail.com>
2022-05-06 23:06:56 +0200

    Added department filter by user login

--- src/main/java/disaster/loss/repository/DepartmentRepository.java
+++ src/main/java/disaster/loss/repository/DepartmentRepository.java
@@ -1,6 +1,9 @@
 package disaster.loss.repository;
 
 import disaster.loss.domain.Department;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +12,6 @@ import org.springframework.stereotype.Repository;
  */
 @SuppressWarnings("unused")
 @Repository
-public interface DepartmentRepository extends JpaRepository<Department, String> {}
+public interface DepartmentRepository extends JpaRepository<Department, String> {
+
+	Page<Department> findByDepartmentId(String departmentId, Pageable pageable);}
--- src/main/java/disaster/loss/service/impl/DepartmentServiceImpl.java
+++ src/main/java/disaster/loss/service/impl/DepartmentServiceImpl.java
@@ -1,15 +1,20 @@
 package disaster.loss.service.impl;
 
-import disaster.loss.domain.Department;
-import disaster.loss.repository.DepartmentRepository;
-import disaster.loss.service.DepartmentService;
 import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import disaster.loss.domain.Department;
+import disaster.loss.repository.DepartmentRepository;
+import disaster.loss.service.DepartmentService;
+import disaster.loss.service.UserService;
+import disaster.loss.service.dto.AdminUserDTO;
 
 /**
  * Service Implementation for managing {@link Department}.
@@ -18,53 +23,71 @@ import org.springframework.transaction.annotation.Transactional;
 @Transactional
 public class DepartmentServiceImpl implements DepartmentService {
 
-    private final Logger log = LoggerFactory.getLogger(DepartmentServiceImpl.class);
+	private final Logger log = LoggerFactory.getLogger(DepartmentServiceImpl.class);
 
-    private final DepartmentRepository departmentRepository;
+	private static class DepartmentResourceException extends RuntimeException {
 
-    public DepartmentServiceImpl(DepartmentRepository departmentRepository) {
-        this.departmentRepository = departmentRepository;
-    }
+		private DepartmentResourceException(String message) {
+			super(message);
+		}
+	}
 
-    @Override
-    public Department save(Department department) {
-        log.debug("Request to save Department : {}", department);
-        return departmentRepository.save(department);
-    }
+	@Autowired
+	UserService userService;
 
-    @Override
-    public Optional<Department> partialUpdate(Department department) {
-        log.debug("Request to partially update Department : {}", department);
+	private final DepartmentRepository departmentRepository;
 
-        return departmentRepository
-            .findById(department.getDepartmentId())
-            .map(existingDepartment -> {
-                if (department.getName() != null) {
-                    existingDepartment.setName(department.getName());
-                }
+	public DepartmentServiceImpl(DepartmentRepository departmentRepository) {
+		this.departmentRepository = departmentRepository;
+	}
 
-                return existingDepartment;
-            })
-            .map(departmentRepository::save);
-    }
+	@Override
+	public Department save(Department department) {
+		log.debug("Request to save Department : {}", department);
+		return departmentRepository.save(department);
+	}
 
-    @Override
-    @Transactional(readOnly = true)
-    public Page<Department> findAll(Pageable pageable) {
-        log.debug("Request to get all Departments");
-        return departmentRepository.findAll(pageable);
-    }
+	@Override
+	public Optional<Department> partialUpdate(Department department) {
+		log.debug("Request to partially update Department : {}", department);
 
-    @Override
-    @Transactional(readOnly = true)
-    public Optional<Department> findOne(String id) {
-        log.debug("Request to get Department : {}", id);
-        return departmentRepository.findById(id);
-    }
+		return departmentRepository.findById(department.getDepartmentId()).map(existingDepartment -> {
+			if (department.getName() != null) {
+				existingDepartment.setName(department.getName());
+			}
 
-    @Override
-    public void delete(String id) {
-        log.debug("Request to delete Department : {}", id);
-        departmentRepository.deleteById(id);
-    }
+			return existingDepartment;
+		}).map(departmentRepository::save);
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public Page<Department> findAll(Pageable pageable) {
+		log.debug("Request to get all Departments");
+
+		AdminUserDTO user = userService.getUserWithAuthorities().map(AdminUserDTO::new)
+				.orElseThrow(() -> new DepartmentResourceException("User could not be found"));
+		
+		Page<Department> page = null;
+		if (user.getAuthorities().contains("ROLE_ADMIN")) {
+			page = departmentRepository.findAll(pageable);
+		} else  {
+			page = departmentRepository.findByDepartmentId(user.getDepartmentId(), pageable);
+		}
+
+		return page;
+	}
+
+	@Override
+	@Transactional(readOnly = true)
+	public Optional<Department> findOne(String id) {
+		log.debug("Request to get Department : {}", id);
+		return departmentRepository.findById(id);
+	}
+
+	@Override
+	public void delete(String id) {
+		log.debug("Request to delete Department : {}", id);
+		departmentRepository.deleteById(id);
+	}
 }
