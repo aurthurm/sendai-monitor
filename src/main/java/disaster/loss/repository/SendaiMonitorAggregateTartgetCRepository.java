129270d8b5c0c3c4528054439e9478cf7f3ee327
Lawrence Chirowodza <chidsza@gmail.com>
2022-05-29 13:44:32 +0200

    Added direct economic loss to sendai monitor report

--- src/main/java/disaster/loss/repository/SendaiMonitorAggregateTartgetCRepository.java
+++ src/main/java/disaster/loss/repository/SendaiMonitorAggregateTartgetCRepository.java
@@ -36,17 +36,19 @@ public interface SendaiMonitorAggregateTartgetCRepository extends JpaRepository<
 			+ "where  d.incident_date BETWEEN :from AND :to", nativeQuery = true)
 	ISendaiAggregateDTO infrastructureLoss(@Param("from") LocalDate dateFrom, @Param("to") LocalDate dateTo);
 
-	@Query(value = "SELECT 'name' AS name, SUM(c.value) AS totalCount,\n"
+	@Query(value = "SELECT 'name' AS name, SUM(h.value) AS totalCount,\n"
 			+ "'C-3 Direct economic loss to all other damaged or destroyed productive assets attributed to disasters.' AS title\n"
-			+ "FROM public.human_population AS c \n" + "where human_population_disaster_category_id in "
-			+ "('186894a2-a5b8-11ec-adfd-90ccdfa85f11')", nativeQuery = true)
+			+ "FROM public.human_population AS h inner join disaster AS d on d.disaster_id = h.disaster_id \n" 
+			+ "where h.human_population_disaster_category_id in ('186894a2-a5b8-11ec-adfd-90ccdfa85f11')"
+			+ " and d.incident_date BETWEEN :from AND :to", nativeQuery = true)
 	ISendaiAggregateDTO damagedOrDestroyedProductiveAssets(@Param("from") LocalDate dateFrom, @Param("to") LocalDate dateTo);
 
 	// C-4 Direct economic loss in the housing sector attributed to disasters.
-	@Query(value = "SELECT 'name' AS name, SUM(c.value) AS totalCount,\n"
+	@Query(value = "SELECT 'name' AS name, SUM(i.value) AS totalCount,\n"
 			+ "'C-4 Direct economic loss in the housing sector attributed to disasters.' AS title\n"
-			+ "FROM public.infrastructure AS c \n" + "where infractructure_type_id in \n"
-			+ "('85f753c8-a495-11ec-b375-90ccdfa85f12')", nativeQuery = true)
+			+ "FROM public.infrastructure AS i inner join disaster AS d on d.disaster_id = i.disaster_id \n" 
+			+ "where infractructure_type_id in ('85f753c8-a495-11ec-b375-90ccdfa85f12') \n"
+			+ " and d.incident_date BETWEEN :from AND :to", nativeQuery = true)
 	ISendaiAggregateDTO econonicLossInHousingSector(@Param("from") LocalDate dateFrom, @Param("to") LocalDate dateTo);
 
 	// C-5 Direct economic loss resulting /from damaged or destroyed critical
